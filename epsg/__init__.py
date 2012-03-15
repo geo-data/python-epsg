b4b9032a1dd64735151bd8891b608a6e9fd38a5a
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2012-03-15 15:51:32 +0000

    Added the Registry class
    This now provides a functional interface to the EPSG registry. It
    remains to create the remainder of the schema objects that have not
    yet been mapped from the GML.

--- epsg/__init__.py
+++ epsg/__init__.py
@@ -1,1 +1,185 @@
+"""
+An API to the EPSG registry
+
+This module provides a way of accessing the data in the EPSG registry
+(http://www.epsg-registry.org). You can: access the latest version
+data in the registry; export the online registry to a local database;
+query the registry for data you are interested in.
+
+See the `Registry` class for more information.
+"""
+
 __version__ = '0.0.1'
+
+import schema, load, service
+
+class Registry(object):
+    """
+    An interface to the EPSG Registry
+
+    This represents a local database copy of the online EPSG
+    registry. Objects in the GML representation of the EPSG export are
+    mapped to their Python representatives and persisted in the
+    database. The SQLAlchemy package is used to interact with the
+    database and as such any database supported by SQLAlchemy can be
+    used for a registry. The default is an in-memory sqlite database
+    if no other database engine is passed in using the `engine`
+    constructor argument. e.g.
+
+    >>> from epsg import Registry
+    >>> registry = Registry()   # use in-memory database
+
+    The registry can be populated from the online EPSG registry at
+    <http://www.epsg-registry.org>.
+
+    >>> registry.create()       # this can take a while!
+
+    An existing database can be reset, which drops and updates it from
+    the registry again:
+
+    >>> registry.reset()
+
+    Objects with specific ids can be retrieved:
+
+    >>> registry.queryByIdentifier('urn:ogc:def:crs:EPSG::27700')
+
+    Along with whole classes of objects:
+
+    >>> from epsg import schema
+    >>> registry.queryByClass(schema.GeodeticDatum)
+
+    More complex queries can be performed by using the SQLAlchemy
+    queries, based on objects in the `schema` module. This is done
+    using the `Repository.session` property which is a
+    `sqlalchemy.orm.Session` instance e.g.
+
+    >>> registry.session.query(schema.Ellipsoid).filter_by(name='Airy 1830').first()
+    """
+
+    def __init__(self, engine=None):
+        from sqlalchemy.orm import sessionmaker
+
+        if engine is None:
+            from sqlalchemy import create_engine
+            # create an in-memory sqlite database as default
+            self.engine = create_engine('sqlite:///:memory:')
+        else:
+            self.engine = engine
+
+        Session = sessionmaker(self.engine)
+        self.session = Session()
+
+    def __repr__(self):
+        return '<Registry(%s)>' % repr(str(self.engine.url))
+
+    def drop(self, useTransaction=True):
+        """
+        Drop the database schema
+
+        This happens within a transaction unless
+        `useTransaction=False`.
+        """
+
+        if useTransaction:
+            self.session.begin(subtransactions=True)
+
+        conn = self.session.connection()
+        schema.Base.metadata.drop_all(conn)
+        
+        if useTransaction:
+            self.session.commit()
+
+    def reset(self, useTransaction=True, update=True):
+        """
+        Drop and recreate the database schema
+        """
+
+        if useTransaction:
+            self.session.begin(subtransactions=True)
+
+        self.drop(useTransaction=False)
+        self.create(useTransaction=False, update=update)
+        
+        if useTransaction:
+            self.session.commit()
+        
+    def create(self, loader=None, useTransaction=True, update=True):
+        """
+        Create the repository database schema
+
+        This deletes all objects in the database and, unless
+        `update=False`, re-populates the database from the online EPSG
+        repository, or from the data in the loader if it is specified
+        using the `loader` parameter.
+        """
+
+        if useTransaction:
+            self.session.begin(subtransactions=True)
+
+        conn = self.session.connection()
+        schema.Base.metadata.create_all(conn)
+
+        if update:
+            self.update(loader, useTransaction=False)
+        
+        if useTransaction:
+            self.session.commit()
+
+    def getLoader(self, gml=None):
+        """
+        Create a loader for EPSG objects
+
+        By default the loader is created from GML returned by the EPSG
+        web service but a custom GML string can be passed in using the
+        `gml` parameter.
+        """
+
+        if gml is None:
+            # connect to the epsg service and retrieve the gml
+            svc = service.Service()
+            svc.connect()
+            gml = svc.export()
+            svc.close()
+
+        # load the gml into the EPSG object structure
+        xml = load.XML.FromString(gml)
+        loader = load.Loader(xml)
+        loader.load()
+
+        return loader
+
+    def update(self, loader=None, useTransaction=True):
+        """
+        Update the local repository database from the EPSG repository
+        """
+
+        if loader is None:
+            loader = self.getLoader()
+        
+        # save the objects to the database
+        if useTransaction:
+            self.session.begin(subtransactions=True)
+
+        self.session.add_all(loader.values())
+            
+        if useTransaction:
+            self.session.commit()
+
+    def queryByIdentifier(self, identifier):
+        """
+        Retrieve an item by its identifier
+
+        e.g.
+        >>> registry.queryByIdentifier('urn:ogc:def:crs:EPSG::27700')
+        """
+        return self.session.query(schema.DictionaryEntry).filter_by(identifier=identifier).first()
+
+    def queryByClass(self, class_):
+        """
+        Retrieve items according to their schema class
+
+        e.g.
+        >>> from epsg import schema
+        >>> registry.queryByClass(schema.GeodeticDatum)
+        """
+        return self.session.query(class_).all()
---
+++ tests/test_registry.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from epsg import Registry, schema, load
+from util import getTestFile
+
+class TestRegistryCreate(unittest.TestCase):
+
+    def setUp(self):
+        self.registry = Registry()
+        xml = load.XML.FromFile(getTestFile())
+        self.loader = load.Loader(xml)
+        self.loader.load()
+
+    def testCreate(self):
+        registry = self.registry
+        # perform the update
+        registry.create(self.loader)
+
+        # check there are the expected number of entries in the database
+        entries = registry.session.query(schema.DictionaryEntry).all()
+        # session.query(schema.DictionaryEntry).count() seems to fail when using `setup.py test`
+        self.assertTrue(len(entries) == len(self.loader))
+
+        # check the polymorphism is working
+        geodetic_datum = registry.session.query(schema.DictionaryEntry).filter_by(identifier='urn:ogc:def:datum:EPSG::6277')[0]
+        self.assertIsInstance(geodetic_datum, schema.GeodeticDatum)
+
+        # check the loading of children is working
+        self.assertIsInstance(geodetic_datum.domainOfValidity, schema.AreaOfUse)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
